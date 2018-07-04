7ba040ece24f58238449b153bd67d41a3766e73f
oliver-gomes <ogomesphoto@gmail.com>
2018-07-04 17:11:31 -0400

    Implementing function CardsInitialize and dd

--- js/app.js
+++ js/app.js
@@ -19,7 +19,7 @@ var openedCards = []; // two max
 var movesCounter = 0;
 var ratingStars = 0;
 var clicks = 0;
-ShuffledCardsList = shuffledCardsInitialize();
+CardsList = CardsInitialize();
 DisplayCards();
 
 // Add event listener
@@ -27,3 +27,42 @@ fireMatcher();
 
 /*-------------------Operation Area----------*/
 
+function CardsInitialize(){
+    var domCards = [];
+    domCards = document.getElementsByClassName("card");
+
+    dd(domCards, 'This cards exists in the dom right now', 'c')
+//    console.log(domCards);
+}
+
+function dd(obj, string = 'Your target', mode = 'l' ){
+    switch(mode){
+        case 'l':
+                var out = "";
+                for(var i in obj){
+                    out += i + " : " + obj[i] + "\n";
+                }
+                alert(string + " : " + out);
+            break;
+        case 'c':
+            console.log(string);
+            console.log(obj);
+            break;
+    }
+}
+function DisplayCards(){
+
+}
+
+function fireMatcher(card){
+    displaySymbol(card);
+    markedOpened(card);
+}
+
+function displaySymbol(){
+
+}
+
+function markedOpened(card){
+
+}
